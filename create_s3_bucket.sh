970da1b957e802f7c1091f4066425f549b9514b4
Kirill Totok <kirill.totok@stem.com>
2019-10-07 12:05:09 +0300

    Upload execute-psql-script.sh to s3

--- create_s3_bucket.sh
+++ create_s3_bucket.sh
@@ -3,3 +3,4 @@ aws s3api create-bucket --bucket kto-bucket --region us-east-2 --create-bucket-c
 aws s3api put-bucket-versioning --bucket kto-bucket --versioning-configuration Status=Enabled
 aws s3 cp dynamo_script.sh s3://kto-bucket/dynamo_script.sh
 aws s3 cp sql-script.sql s3://kto-bucket/sql-script.sql
+aws s3 cp execute-psql-script.sh s3://kto-bucket/execute-psql-script.sh
